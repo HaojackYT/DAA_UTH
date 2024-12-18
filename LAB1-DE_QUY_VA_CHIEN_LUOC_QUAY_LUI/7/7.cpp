7b5a0cf1171e60e54f3ed89e2df97733f55f02dd
Tăng Chí Hào <145805739+HaojackYT@users.noreply.github.com>
2024-12-18 07:18:31 +0700

    Update 7.cpp

--- LAB1-DE_QUY_VA_CHIEN_LUOC_QUAY_LUI/7/7.cpp
+++ LAB1-DE_QUY_VA_CHIEN_LUOC_QUAY_LUI/7/7.cpp
@@ -16,24 +16,31 @@ void permutation(int i, int N, int X[], bool used[], int tmp[]) {
     for (int j = 0; j < N; j++) {
         // Kiểm tra có thể gán X[i] = j hay không ?
         // Kiểm tra phần tử thứ j đã được sử dụng chưa
-        if1  (!used[j]) {
+        if (!used[j]) {
             // Đánh dấu phần tử thứ j đã được sử dụng
             used[j] = true;
             tmp[i] = X[j];
-            // Kiểm tra đã đủ N 
+            // Kiểm tra đã đủ N phần tử cho một hoán vị hay chưa?
             if (i == N - 1) {
                 printAnswer(N, tmp);
-            } else {
+            }
+            // Nếu chưa đủ, gọi lại hàm quay lui để xây dựng phần tử tiếp theo
+            else {
                 permutation(i + 1, N, X, used, tmp);
             }
+            // Quay lui, đánh dấu phần tử thứ j chưa được sử dụng
             used[j] = false;
         }
     }
 }
 
 int main() {
-    int N;
+    int N; // Biến 'N' lưu số lượng phần tử của mảng
     cout << "Nhap vao so luong phan tu: "; cin >> N;
+    
+    
+    
+    // Mảng 'X' lưu giá trị của các phần tử, 'tmp'
     int X[N], tmp[N]; bool used[N];
     for (int i = 0; i < N; i++) {
         int tmp;
